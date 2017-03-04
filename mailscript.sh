653b92af762d1cab967fd258dddb358157b65ba9
reno777 <seth.rasmussen13@gmail.com>
2017-03-04 02:17:28 -0500

    Update mailscript.sh

--- mailscript.sh
+++ mailscript.sh
@@ -12,3 +12,29 @@ mv -f smtpd.key.unencrypted smtpd.key
 openssl req -new -x509 -extensions v3_ca -keyout cakey.pem -out cacert.pem -days 365
 
 sed -i 's/^inet_interfaces = localhost/#inet_interfaces = localhost/' /etc/postfix/main.cf
+
+echo "myhostname = mail.test.com" >> /etc/postfix/main.cf
+echo "mydomain = test.com" >> /etc/postfix/main.cf
+echo "myorigin = $mydomain" >> /etc/postfix/main.cf
+echo "home_mailbox = mail/" >> /etc/postfix/main.cf
+echo "mynetworks = 127.0.0.0/8" >> /etc/postfix/main.cf
+echo "inet_interfaces = all" >> /etc/postfix/main.cf
+echo "mydestination = $myhostname, localhost.$mydomain, localhost, $mydomain" >> /etc/postfix/main.cf
+echo "smtpd_sasl_auth_enable = yes" >> /etc/postfix/main.cf
+echo "smtpd_sasl_type = cyrus" >> /etc/postfix/main.cf
+echo "smtpd_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
+echo "broken_sasl_auth_clients = yes" >> /etc/postfix/main.cf
+echo "smtpd_sasl_authenticated_header = yes" >> /etc/postfix/main.cf
+echo "smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks,reject_unauth_destination" >> /etc/postfix/main.cf
+echo "smtpd_tls_auth_only = no" >> /etc/postfix/main.cf
+echo "smtp_use_tls = yes" >> /etc/postfix/main.cf
+echo "smtpd_use_tls = yes" >> /etc/postfix/main.cf
+echo "smtp_tls_note_starttls_offer = yes" >> /etc/postfix/main.cf
+echo "smtpd_tls_key_file = /etc/postfix/ssl/smtpd.key" >> /etc/postfix/main.cf
+echo "smtpd_tls_cert_file = /etc/postfix/ssl/smtpd.crt" >> /etc/postfix/main.cf
+echo "smtpd_tls_CAfile = /etc/postfix/ssl/cacert.pem" >> /etc/postfix/main.cf
+echo "smtpd_tls_received_header = yes" >> /etc/postfix/main.cf
+echo "smtpd_tls_session_cache_timeout = 3600s" >> /etc/postfix/main.cf
+echo "tls_random_source = dev:/dev/urandom" >> /etc/postfix/main.cf
+
+echo "http://www.krizna.com/centos/setup-mail-server-in-centos-6/"
