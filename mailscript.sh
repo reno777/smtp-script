29de5536e61aa1a180828822f4c1acda44666bb2
reno777 <seth.rasmussen13@gmail.com>
2017-03-04 02:04:22 -0500

    Update mailscript.sh

--- mailscript.sh
+++ mailscript.sh
@@ -1,4 +1,14 @@
 !#/bin/bash
+yum install postfix -y
+yum -y install cyrus-sasl cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-md5 cyrus-sasl-plain
 mkdir /etc/postfix/ssl
 cd /etc/postfix/ssl
 openssl genrsa -des3 -rand /etc/hosts -out smtpd.key 1024
+chmod 600 smtpd.key
+openssl req -new -key smtpd.key -out smtpd.csr
+openssl x509 -req -days 365 -in smtpd.csr -signkey smtpd.key -out smtpd.crt
+openssl rsa -in smtpd.key -out smtpd.key.unencrypted
+mv -f smtpd.key.unencrypted smtpd.key
+openssl req -new -x509 -extensions v3_ca -keyout cakey.pem -out cacert.pem -days 365
+
+sed -i 's/^inet_interfaces = localhost/#inet_interfaces = localhost/' /etc/postfix/main.cf
