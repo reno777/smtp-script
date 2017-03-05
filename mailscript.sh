50587c5ced3b16816f4b41f310e1e77d0bbefe63
reno777 <seth.rasmussen13@gmail.com>
2017-03-05 00:49:22 -0500

    Update mailscript.sh

--- mailscript.sh
+++ mailscript.sh
@@ -57,6 +57,30 @@ echo '    }' >> /etc/dovecot/dovecot.conf
 echo '}' >> /etc/dovecot/dovecot.conf
 echo 'ssl = no' >> /etc/dovecot/dovecot.conf
 
+#iptables
+mv /etc/sysconfig/itpables /etc/sysconfig/iptables.bak
+touch /etc/sysconfig/iptables
+
+echo ':INPUT DROP [0:0]' >> /etc/sysconfig/iptables
+echo ':FORWARD DROP [0:0]' >> /etc/sysconfig/iptables
+echo ':OUTPUT ACCEPT [0:0]' >> /etc/sysconfig/iptables
+echo '-A INPUT -m state --state ESTABLSIHED,RELATED -j ACCEPT' >> /etc/sysconfig/iptables
+echo '-A INPUT -m state --state NEW -m tcp -p tcp --dport 25 -j ACCEPT' >> /etc/sysconfig/iptables
+echo '-A INPUT -m state --state NEW -m tcp -p tcp --dport 143 -j ACCEPT' >> /etc/sysconfig/iptables
+echo '-A INPUT -j REJECT' >> /etc/sysconfig/iptables
+echo 'COMMIT' >> /etc/sysconfig/iptables
 
 systemctl stop firewalld
 systemctl disable firewalld
+systemctl restart iptables
+systemctl status iptables
+sleep 5
+
+#chkconfig/network
+chkconfig network on
+systemctl restart network
+sleep 5
+
+#end
+gnome-terminal
+ngrep -d any port 25
